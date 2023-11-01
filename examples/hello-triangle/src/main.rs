f486e23627000fa9f4429ddaa779d0fdd75e4aec
Kaifuny <superbiger.github@gmail.com>
2023-11-01 15:10:08 +0800

    create local window

--- examples/hello-triangle/src/main.rs
+++ examples/hello-triangle/src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::EventLoop,
+    window::WindowBuilder,
+};
+
+fn main() -> Result<(), impl std::error::Error> {
+    let event_loop = EventLoop::new().unwrap();
+
+    let window = WindowBuilder::new()
+        .with_title("Hello, world!")
+        .build(&event_loop)
+        .unwrap();
+
+    event_loop.run(move |event, elwt| {
+        match event {
+            Event::WindowEvent { event, window_id } if window_id == window.id() => match event {
+                WindowEvent::CloseRequested => elwt.exit(),
+                WindowEvent::RedrawRequested => {
+                    // Notify the windowing system that we'll be presenting to the window.
+                    window.pre_present_notify();
+                }
+                _ => (),
+            },
+            Event::AboutToWait => {
+                window.request_redraw();
+            }
+
+            _ => (),
+        }
+    })
 }
