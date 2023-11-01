9bbf2c04d8df872ee765b72e0dc4b93ae8f7833b
Kaifuny <superbiger.github@gmail.com>
2023-11-01 23:45:18 +0800

    add wasm32 config

--- examples/hello-triangle/src/main.rs
+++ examples/hello-triangle/src/main.rs
@@ -1,17 +1,10 @@
 use winit::{
     event::{Event, WindowEvent},
     event_loop::EventLoop,
-    window::WindowBuilder,
+    window::{Window, WindowBuilder},
 };
 
-fn main() -> Result<(), impl std::error::Error> {
-    let event_loop = EventLoop::new().unwrap();
-
-    let window = WindowBuilder::new()
-        .with_title("Hello, world!")
-        .build(&event_loop)
-        .unwrap();
-
+async fn run(event_loop: EventLoop<()>, window: Window) {
     event_loop.run(move |event, elwt| {
         match event {
             Event::WindowEvent { event, window_id } if window_id == window.id() => match event {
@@ -29,4 +22,34 @@ fn main() -> Result<(), impl std::error::Error> {
             _ => (),
         }
     })
+    .unwrap();
+}
+
+fn main() {
+    let event_loop = EventLoop::new().unwrap();
+    let window = WindowBuilder::new()
+        .with_title("Hello, world!")
+        .build(&event_loop)
+        .unwrap();
+    #[cfg(not(target_arch = "wasm32"))]
+    {
+        env_logger::init();
+        pollster::block_on(run(event_loop, window));
+    }
+    #[cfg(target_arch = "wasm32")]
+    {
+        std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+        console_log::init().expect("could not initialize logger");
+        use winit::platform::web::WindowExtWebSys;
+        // On wasm, append the canvas to the document body
+        web_sys::window()
+            .and_then(|win| win.document())
+            .and_then(|doc| doc.body())
+            .and_then(|body| {
+                body.append_child(&web_sys::Element::from(window.canvas().unwrap()))
+                    .ok()
+            })
+            .expect("couldn't append canvas to document body");
+        wasm_bindgen_futures::spawn_local(run(event_loop, window));
+    }
 }
